8f180f31f9bb29cab4f46c0c5fb44977cda5d1ab
Danilo Hernandes de Souza <dhsouza@stefanini.com>
2018-08-21 23:22:25 -0300

    15.2. Prototipando a pesquisa da cerveja

--- brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,10 +2,12 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import com.algaworks.brewer.repository.Cervejas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +28,9 @@ public class CervejasController {
 	@Autowired
 	private CadastroCervejaService cadastroCervejaService;
 
+	@Autowired
+	private Cervejas cervejas;
+
 	@RequestMapping("/cervejas/novo")
 	public ModelAndView novo(Cerveja cerveja) {
 		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
@@ -44,6 +49,17 @@ public class CervejasController {
 		cadastroCervejaService.salvar(cerveja);
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		return new ModelAndView("redirect:/cervejas/novo");
+	}
+
+	@GetMapping("/cervejas")
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		mv.addObject("cervejas", cervejas.findAll());
+
+		return mv;
 	}
 	
 }
